6f6630f76fc86044b6fc4395967e50d76294baee
David Orozco <orozco.davidj02@gmail.com>
2021-07-15 18:30:45 -0500

    feat 0x00-ls: fixing betty for main file

--- 0x00-ls/hls.c
+++ 0x00-ls/hls.c
@@ -1,5 +1,13 @@
 #include "hls.h"
 
+/**
+ * main - Entry point for the hls command
+ *
+ * @argc: Number of arguments
+ * @argv: Pointer to pointer of arguments
+ *
+ * Return: Errno code or 0 if everything its ok
+ **/
 int main(int argc, char **argv)
 {
 	general_t *info;
